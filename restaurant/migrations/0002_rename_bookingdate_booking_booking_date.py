4d4e4005cfd5da56b67b0ce443db4f85743d8784
Zmrh007 <65283642+Zmrh007@users.noreply.github.com>
2024-09-06 16:45:12 +0530

    Initial commit

---
+++ restaurant/migrations/0002_rename_bookingdate_booking_booking_date.py
@@ -0,0 +1,17 @@
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('restaurant', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='booking',
+            old_name='BookingDate',
+            new_name='booking_date',
+        ),
+    ]
